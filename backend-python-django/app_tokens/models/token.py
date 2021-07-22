6964979bed17c4c8adec16f9ca823f968ffcebad
Ivan Usalko <i.usalko@itransition.com>
2021-07-22 15:17:01 +0300

    # Initial implementation
    Django backend development
      -  There are two api endpoints implemented (Token and Token/Validation).

--- backend-python-django/app_tokens/models/token.py
+++ backend-python-django/app_tokens/models/token.py
@@ -1,9 +1,9 @@
-import uuid
+from uuid import uuid4
 from django.db import models
 
 
 class Token(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     created = models.DateTimeField(help_text='Timestamp when token created')
 
     class Meta:
--- backend-python-django/app_tokens/views/token_view_set.py
+++ backend-python-django/app_tokens/views/token_view_set.py
@@ -1,9 +1,18 @@
+from os import urandom
+from threading import Lock
+from random import seed
+from random import getrandbits
+from uuid import UUID
+from datetime import datetime
 from rest_framework import mixins, viewsets, permissions
 from app_tokens.models import Token
 from app_tokens.serializers import TokenSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from common import AutoDocStringSchema
+
+
+RANDOM_SEED_LOCK_TIMEOUT_IN_SECONDS = 1
 
 
 class TokenViewSet(mixins.ListModelMixin,
@@ -15,11 +24,12 @@ class TokenViewSet(mixins.ListModelMixin,
     serializer_class = TokenSerializer
 
     schema = AutoDocStringSchema()
+    __lock = Lock()
 
     def list(self, request, *args, **kwargs):
         '''
         get:
-          description: Generate new token
+          description: Request new token
           summary: Create new token by two hashes
           parameters:
             - name: user-agent-hash
@@ -44,11 +54,24 @@ class TokenViewSet(mixins.ListModelMixin,
         random_seed = request.query_params['seed']
         if not user_agent_hash:
             raise Exception(
-                f'Invalid request random user-agent-hash must be determined in query string')
+                f'Invalid request user-agent-hash must be determined in query string')
         if not random_seed:
             raise Exception(
                 f'Invalid request random random-seed must be determined in query string')
-        return Response('Ok')
+        token = Token()
+
+        self.__lock.acquire(timeout=RANDOM_SEED_LOCK_TIMEOUT_IN_SECONDS)
+        try:
+            seed(int.from_bytes(int(user_agent_hash).to_bytes(4, 'big') +
+                                int(random_seed).to_bytes(4, 'big'), 'big'))
+            token.id = UUID(bytes=(int.from_bytes(urandom(16), 'big') ^ getrandbits(128)).to_bytes(16, 'big'), version=4)
+            token.created = datetime.now()  # Use local datetime
+        finally:
+            self.__lock.release()
+
+        token.save()
+
+        return Response(str(token.id))
 
     @action(detail=False, methods=['get'], url_path='Validation')
     def validation(self, request):
@@ -56,5 +79,21 @@ class TokenViewSet(mixins.ListModelMixin,
         get:
           description: Check that token is registered
           summary: Check if token is exist in database
+          parameters:
+            - name: token
+              in: query
+              description: String value as uuid 430d5a05-78d3-3f2c-9a9c-397db64b66c5.
+              required: true
+              schema:
+                  type: string
+          responses:
+            '200':
+                description: Boolean value
+                content:
+                  'application/json': true
         '''
-        ...
+        token = request.query_params['token']
+        if not token:
+            raise Exception(
+                f'Invalid request token must be determined in query string')
+        return Response(bool(Token.objects.filter(pk=token).exists()))
